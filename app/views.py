e560c4f240916f264f268f0a927a4468dd4854aa
Tony Rogers <tony.rogers@rackspace.com>
2013-12-06 14:46:40 -0600

    remove displaying password

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for Username="' + form.username.data + '", Password="' + form.password.data + '", remember_me=' + str(form.remember_me.data))
+        flash('Login requested for Username="' + form.username.data + '", Password="*******", remember_me=' + str(form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', 
         title = 'Sign In',
